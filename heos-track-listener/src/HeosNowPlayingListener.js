1d8c1ddd02ca314a700d085021fd4f1d8a634a35
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2019-07-14 12:18:38 +0300

    Listener: improved support for multiple devices & players - store now playing by player pid

--- heos-track-listener/src/HeosNowPlayingListener.js
+++ heos-track-listener/src/HeosNowPlayingListener.js
@@ -22,7 +22,7 @@ class HeosTrackListener {
           .on({commandGroup: 'player', command: 'get_now_playing_media'}, HeosTrackListener.handleTrack)
           .on({commandGroup: 'player', command: 'get_player_info'}, HeosTrackListener.handlePlayerInfo)
           .on({commandGroup: 'event', command: 'player_now_playing_changed'}, (data) => {
-            const pid = data.heos.message.split('=').pop();
+            const pid = HeosTrackListener.getPlayerPid(data);
             connection.write('player', 'get_player_info', { pid: pid })
               .write('player', 'get_now_playing_media', { pid: pid });
           });
@@ -33,68 +33,82 @@ class HeosTrackListener {
     axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
   }
 
+  static getPlayerPid(data) {
+    return data.heos.message.split('=').pop();
+  }
+
   static handlePlayerInfo(data) {
     HeosTrackListener.playerName = data.payload.name;
   }
 
   static async handleTrack(data) {
+    const pid = HeosTrackListener.getPlayerPid(data);
+
     data = data.payload;
     console.log(`${HeosTrackListener.playerName || 'HEOS DEVICE'} IS NOW PLAYING`, data);
 
     const now = moment().unix();
 
     // HEOS device emits duplicate 'player_now_playing_changed' events when manually changing track. Prevent scrobbling those.
-    if (HeosTrackListener.nowPlaying &&
-        HeosTrackListener.nowPlaying.startedAt &&
-        now - HeosTrackListener.nowPlaying.startedAt > 10) {
-      await HeosTrackListener.submitTrack();
+    if (HeosTrackListener.nowPlaying[pid] &&
+        HeosTrackListener.nowPlaying[pid].startedAt &&
+        now - HeosTrackListener.nowPlaying[pid].startedAt > 10) {
+      await HeosTrackListener.submitTrack(pid);
     }
 
     if (data.type === 'song' || !process.env.SUBMIT_ONLY_USB_AND_NETWORK) {
-      HeosTrackListener.nowPlaying = new HeosTrack(data.song, data.artist, data.album, now);
-      await HeosTrackListener.submitNowPlaying();
+      HeosTrackListener.nowPlaying[pid] = new HeosTrack(data.song, data.artist, data.album, now);
+      await HeosTrackListener.submitNowPlaying(pid);
     }
   }
 
-  static async submitTrack() {
-    if (HeosTrackListener.nowPlaying &&
-        HeosTrackListener.nowPlaying.title &&
-        HeosTrackListener.nowPlaying.artist) {
+  static async submitTrack(pid) {
+    if (HeosTrackListener.nowPlaying[pid] &&
+        HeosTrackListener.nowPlaying[pid].title &&
+        HeosTrackListener.nowPlaying[pid].artist) {
       const finishedAt = moment().unix();
-      HeosTrackListener.unsubmittedTracks[finishedAt] = HeosTrackListener.nowPlaying;
-      HeosTrackListener.nowPlaying = null;
+
+      HeosTrackListener.unsubmittedTracks[pid][finishedAt] = HeosTrackListener.nowPlaying[pid];
+      HeosTrackListener.nowPlaying[pid] = null;
 
       await Promise.all(Object.keys(HeosTrackListener.unsubmittedTracks)
-        .map((key) => {
-          return axios({
-            method: 'POST',
-            url: `${process.env.HEOS_SUBMIT_BASE_URL}/scrobble`,
-            data: {
-              ...HeosTrackListener.unsubmittedTracks[key],
-              finishedAt: key
-            }
-          })
-            .then((response) => {
-              console.log('TRACK SUBMIT RESPONSE', response.data);
-              delete HeosTrackListener.unsubmittedTracks[key];
+        .map((pid) => {
+          Object.keys(HeosTrackListener.unsubmittedTracks[pid])
+            .map((finishedAt) => {
+              return axios({
+                method: 'POST',
+                url: `${process.env.HEOS_SUBMIT_BASE_URL}/scrobble`,
+                data: {
+                  ...HeosTrackListener.unsubmittedTracks[pid][finishedAt],
+                  finishedAt: finishedAt
+                }
+              })
+                .then((response) => {
+                  console.log('TRACK SUBMIT RESPONSE', response.data);
+                  delete HeosTrackListener.unsubmittedTracks[pid][key];
+                })
+                .catch((err) => {
+                  if (err.response) {
+                    console.error('TRACK SUBMIT ERROR', err.response.status, err.response.data);
+                  } else {
+                    console.error('TRACK SUBMIT ERROR', err);
+                  }
+                });
             })
-            .catch((err) => {
-              console.error('TRACK SUBMIT ERROR', err.response.status, err.response.data);
-            });
         })
       );
 
     }
   }
 
-  static async submitNowPlaying() {
-    if (HeosTrackListener.nowPlaying &&
-        HeosTrackListener.nowPlaying.title &&
-        HeosTrackListener.nowPlaying.artist) {
+  static async submitNowPlaying(pid) {
+    if (HeosTrackListener.nowPlaying[pid] &&
+        HeosTrackListener.nowPlaying[pid].title &&
+        HeosTrackListener.nowPlaying[pid].artist) {
       return axios({
         method: 'POST',
         url: `${process.env.HEOS_SUBMIT_BASE_URL}/nowPlaying`,
-        data: HeosTrackListener.nowPlaying,
+        data: HeosTrackListener.nowPlaying[pid],
       })
         .catch((err) => {
           console.error('NOW PLAYING SUBMIT ERROR', err.response.status, err.response.data);
@@ -104,7 +118,7 @@ class HeosTrackListener {
 }
 
 HeosTrackListener.playerName = null;
-HeosTrackListener.nowPlaying = null;
+HeosTrackListener.nowPlaying = {};
 HeosTrackListener.unsubmittedTracks = {};
 
 module.exports = HeosTrackListener;
