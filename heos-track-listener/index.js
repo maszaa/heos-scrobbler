4c4ed977f6d6b65d100bbef4a79c2e00234ca6bc
Samuli Kohomäki <samuli.kohomaki@gmail.com>
2019-08-05 18:18:23 +0300

    Listener: added signal handler

--- heos-track-listener/index.js
+++ heos-track-listener/index.js
@@ -3,6 +3,19 @@ const mongoose = require('mongoose');
 const HeosNowPlayingListener = require('./heos/HeosNowPlayingListener');
 const HeosPlayer = require('./models/HeosPlayer');
 
+async function handleSignals(signal, connections) {
+  console.log(`Received ${signal}, closing connections to HEOS devices`);
+
+  await Promise.all(Object.keys(connections)
+    .map((key) => connections[key].close()
+      .catch(console.error))
+  )
+    .catch(console.error);
+
+  console.log('Closed HEOS connections');
+  process.exit(0);
+}
+
 async function initialize() {
   await mongoose.connect(
     `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
@@ -14,11 +27,14 @@ async function initialize() {
 
   const heosNowPlayingListener = new HeosNowPlayingListener();
 
-  await heosNowPlayingListener.initialize()
+  const connections = await heosNowPlayingListener.initialize()
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
+
+  process.on('SIGTERM', (signal) => handleSignals(signal, connections));
+  process.on('SIGINT', (signal) => handleSignals(signal, connections));
 }
 
 module.exports = initialize();
